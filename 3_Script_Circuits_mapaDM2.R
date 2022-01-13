fc621401367c77c503e1938a619a9bdac028ad6c
Ana Lago <89092066+AnaLagoSampedro@users.noreply.github.com>
2022-01-13 15:06:53 +0100

    Rename Script_Circuits_mapaDM2.R to 3_Script_Circuits_mapaDM2.R

--- 3_Script_Circuits_mapaDM2.R
+++ 3_Script_Circuits_mapaDM2.R
@@ -1,26 +1,26 @@
 ################################################################################
-############### Construcci髇 del fichero Circuits "Diabetes2.rds" ##################
-###################### para el aprendizaje autom醫ico ##########################
+############### Construcci贸n del fichero Circuits "Diabetes2.rds" ##################
+###################### para el aprendizaje autom谩tico ##########################
 ################################################################################
 
 #Generar un data.frame con 1098 filas correspondientes a todos los circuitos y 
-#sus efectores en hipathia.Estos ir醤 en una columna denominada "hipathia", 
-#esta informaci髇 se obtiene tras aplicar la funci髇 get_paths_data() de hipathia.
+#sus efectores en hipathia.Estos ir谩n en una columna denominada "hipathia", 
+#esta informaci贸n se obtiene tras aplicar la funci贸n get_paths_data() de hipathia.
 #(Se puede obtener de cualquiera de los dos ficheros GSE164416 y GTEx).
 
 #A partir de GTEx, por ejemplo, abrir el data.frame con los 1098 subpathways que 
-#hay en hipathia sacado tras aplicar la funci髇 get_paths_data(). 
+#hay en hipathia sacado tras aplicar la funci贸n get_paths_data(). 
 #Obtenido del Script_GTEx_hipathia.R
 circuits_hipathia <- readRDS("~path/circuits_hipathia.rds")
 head(circuits_hipathia)
 dim(circuits_hipathia)
-#1098 subpathways que est醤 en hipathia de 79 rutas fisiol骻icas.
+#1098 subpathways que est谩n en hipathia de 79 rutas fisiol贸gicas.
 colnames(circuits_hipathia) <- "hipathia" #cambiar nombre de la columna
 
 #Ahora, generar la segunda columna del fichero circuits "in_disease":
 #con el resultado de los pathways diferencialmente activados para la enfermedad,
-#obtenido del objeto comp obtenido en GSE164416 tras aplicar el an醠isis de 
-#activaci髇 diferencial de circuitos (funci髇 do_wilcoxon(); Script_HipathiaWilcoxon.R)
+#obtenido del objeto comp obtenido en GSE164416 tras aplicar el an谩lisis de 
+#activaci贸n diferencial de circuitos (funci贸n do_wilcoxon(); Script_HipathiaWilcoxon.R)
 comp <- read.csv("~path/comp.txt", sep = "\t", header = TRUE)
 head(comp)
 circuits_hipathia$in_disease<-"FALSE"
@@ -31,7 +31,7 @@ head(circuits_hipathia)
 #Guardar este fichero circuits, como rds y feather.
 write.table(circuits_hipathia, file = "Diabetes2.txt",sep = "\t", row.names = FALSE, col.names = TRUE)
 saveRDS(circuits_hipathia, file = "Diabetes2.rds")
-#Conversi髇 a .feather
+#Conversi贸n a .feather
 #install.packages("feather")
 library(feather)
 write_feather(circuits_hipathia, path = "Diabetes2.feather")
